578ac9a0f417954f49770ee63c4ae5911c0fd686
yk271 <29210205+yk271@users.noreply.github.com>
2026-01-21 17:34:43 +0800

    update

--- immortalwrt-25.12/diy-part2.sh
+++ immortalwrt-25.12/diy-part2.sh
@@ -30,16 +30,18 @@ rm -rf ./feeds/luci/applications/{luci-app-passwall,luci-app-passwall2}
 # 添加 Passwall
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git -b main ./package/passwall_packages
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall.git -b main ./package/passwall_luci
+
+cd package/passwall_luci
+git checkout 97921a9178f10be41e31bdabcef69c0ca444adb4
+cd -
+
+# Passwall DIY
 wget https://raw.githubusercontent.com/yk271/Actions-OpenWrt/refs/heads/main/package-diy/passwall/direct_host -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/direct_host
 wget https://raw.githubusercontent.com/yk271/Actions-OpenWrt/refs/heads/main/package-diy/passwall/proxy_host -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/proxy_host
 wget https://raw.githubusercontent.com/yk271/Actions-OpenWrt/refs/heads/main/package-diy/passwall/block_host -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/block_host
 wget https://raw.githubusercontent.com/yk271/Actions-OpenWrt/refs/heads/main/package-diy/passwall/chnlist -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/chnlist
 wget https://raw.githubusercontent.com/yk271/Actions-OpenWrt/refs/heads/main/package-diy/passwall/gfwlist -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/gfwlist
 wget https://raw.githubusercontent.com/yk271/Actions-OpenWrt/refs/heads/main/package-diy/passwall/0_default_config -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/0_default_config
-
-cd package/passwall_luci
-git checkout 97921a9178f10be41e31bdabcef69c0ca444adb4
-cd -
 
 # Passwall 补丁
 cd package/passwall_luci
